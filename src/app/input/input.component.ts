8fb6ede1f2c2b5439908dc3b3907b0cd7e1b6613
alaashaher <alaashaher12@gmail.com>
2019-04-27 23:26:18 +0200

    show and delete li

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -5,28 +5,40 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './input.component.html',
     styleUrls: ['./input.component.scss']
 })
-export class InputComponent {
+export class InputComponent implements OnInit {
     hasError: any;
     inputValue: any;
-    // ListValue: [];
+    ListValue: any;
     ShowList: boolean;
-    ListValue: any[] = new Array();
+    // ListValue: any[] = new Array();
     constructor(
     ) {
         this.hasError = false;
         this.ShowList = false;
+        // this.ListValue = ['alaa'];
+        // this.ListValue = [{
+        //     id: 1,
+        //     name: 'Boyer Stanley',
+        //   }];
+    }
+    ngOnInit() {
+        this.ListValue = [];
+    }
+    addValue() {
+        this.ListValue.push(this.inputValue);
     }
     addTask() {
         if (!this.inputValue) {
             this.hasError = true;
         } else {
-            this.ListValue.push(this.inputValue);
-            console.log(this.ListValue);
+            this.addValue();
             this.ShowList = true;
             this.hasError = false;
+            console.log(this.ListValue, this.ShowList);
         }
     }
     delete(value) {
+        console.log(value);
         const index = this.ListValue.indexOf(value, 0);
         if (index > -1) {
             this.ListValue.splice(index, 1);
