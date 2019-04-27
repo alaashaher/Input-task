9e7983d59afd6da85f0359d8cf37f7511f871adb
alaashaher <alaashaher12@gmail.com>
2019-04-27 13:57:53 +0200

    update

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -8,16 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class InputComponent {
     hasError: any;
     inputValue: any;
+    // ListValue: [];
+    ShowList: boolean;
+    ListValue: any[] = new Array();
     constructor(
     ) {
         this.hasError = false;
+        this.ShowList = false;
     }
     addTask() {
         if (!this.inputValue) {
             this.hasError = true;
-            console.log(this.hasError);
         } else {
+            this.ListValue.push(this.inputValue);
+            console.log(this.ListValue);
+            this.ShowList = true;
             this.hasError = false;
         }
+    }
+    delete(value) {
+        const index = this.ListValue.indexOf(value, 0);
+        if (index > -1) {
+            this.ListValue.splice(index, 1);
+        }
+        console.log(this.ListValue);
     }
 }
